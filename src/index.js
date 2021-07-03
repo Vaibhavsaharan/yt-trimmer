4b89fdb9e874d072ab7811330b86ff1a30d1e382
Vaibhavsaharan <35521669+Vaibhavsaharan@users.noreply.github.com>
2021-07-03 15:09:52 +0530

    yt trimmer api integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+const express = require('express');
 const robots = require('./robots');
 
-const start = async () => {
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/trim', async (req,res) =>{
+  const info = req.body;
+  console.log(info);
   try {
-    const content = await robots.input();
-    await robots.info(content);
-    await robots.download(content);
+    await robots.info(info);
+    await robots.download(info);
+    res.send('Done');
   } catch (err) {
     console.log('Error waiting for robots:\n\n ', err);
+    res.send('Error!')
   }
-};
+})
 
-start();
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Express listening on ${port}`);
